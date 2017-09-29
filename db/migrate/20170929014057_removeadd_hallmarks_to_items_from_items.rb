85745a4b4fcfbed35275e8398fcdb5a77dbec133
Mark Hannah <hannahm7@msn.com>
2017-09-28 22:56:34 -0400

    Expanded Attribute Logic

--- db/migrate/20170929014057_removeadd_hallmarks_to_items_from_items.rb
+++
@@ -1,4 +0,0 @@
-class RemoveaddHallmarksToItemsFromItems < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/migrate/20170929015325_change_bracelet_closure_to_braceletclosure.rb
+++
@@ -1,5 +0,0 @@
-class ChangeBraceletClosureToBraceletclosure < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :items, :BraceletClosure, :braceletclosure
-  end
-end
