7c80617092972ae0ce9451d6d6cef253d0a21efd
Mark Hannah <hannahm7@msn.com>
2017-10-02 09:50:57 -0400

    Added Stone Atttributes

--- db/migrate/20171002133013_change_caratweight_to_float.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCaratweightToFloat < ActiveRecord::Migration[5.1]
-  def change
-    change_column :items, :braceletcstonecaratweight, :float
-  end
-end
--- db/migrate/20171002133155_change_scaratweight_to_float.rb
+++
@@ -1,5 +0,0 @@
-class ChangeScaratweightToFloat < ActiveRecord::Migration[5.1]
-  def change
-    change_column :items, :braceletsstonecaratweight, :float
-  end
-end
