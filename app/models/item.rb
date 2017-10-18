7c0a31d6d3eb613df6a07727ed1b5546c7b5331d
Mark Hannah <hannahm7@msn.com>
2017-10-17 22:28:16 -0400

    Updated with andrea feedback

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,14 @@ class Item < ApplicationRecord
   before_validation do |item|
   item.ringmetaltype.reject!(&:blank?) if item.ringmetaltype
   item.ringothermetal.reject!(&:blank?) if item.ringothermetal
+  item.ringcstonetype.reject!(&:blank?) if item.ringcstonetype
+
   end
 
 
   serialize :ringmetaltype
   serialize :ringothermetal
+  serialize :ringcstonetype
 
   has_many :item_detail
 
