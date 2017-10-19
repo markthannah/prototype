026e3e0a5cae5de513f064f186b0e7fc82d940d1
Mark Hannah <hannahm7@msn.com>
2017-10-19 12:43:39 -0400

    Added fashion back

--- db/migrate/20171019161845_change_costumerdislaimer_to_boolen.rb
+++
@@ -1,6 +0,0 @@
-class ChangeCostumerdislaimerToBoolen < ActiveRecord::Migration[5.1]
-  def change
-    change_column :items, :ringcostumegemdisclaimer, :boolean
-    change_column :items, :ringcostumegeodisclaimer, :boolean
-  end
-end
